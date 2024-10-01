aaa1657a0b57ef3923e5ed8e6693624741542299
ravi07111999 <ravi07111999@gmail.com>
2024-10-01 12:00:35 +0530

    CV

---
+++ C Program/Greater than 2 number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter number: ");
+    scanf("%d",&a);
+    printf("Enter number: ");
+    scanf("%d",&b);
+    if(a>b)
+    {
+        printf("%d is greater number");
+    }
+    else
+    {
+        printf("%d is greater number");
+    }
+    return 0;
+}
