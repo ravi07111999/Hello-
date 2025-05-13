20ca15e0f7e091948c598e2ab834e62b0736a9b9
Ravi singh <30411742+ravi07111999@users.noreply.github.com>
2025-05-13 13:24:27 +0530

    Class work

---
+++ College work/HTML & CSS & Javascript1/Day 3/Day3.js
@@ -0,0 +1,30 @@
+// function testFunction(x)
+// {
+//     console.log(x);
+// }
+// //testFunction(5);
+// testFunction("Hello");
+
+// function testFunction(x,y)
+// {
+//     return x+y;
+// }
+// let c = testFunction(5,3);
+// console.log(c);
+
+
+// function abc(a,b="Nothing")
+// {
+//     console.log(a);
+//     console.log(b);
+// }
+// abc();
+// abc(10);
+// abc(10,"Chirag");
+
+
+let t = function()
+{
+    console.log("This is annonymous function");
+}
+t();
---
+++ College work/HTML & CSS & Javascript1/Day 6/Day6.js
@@ -0,0 +1,8 @@
+function Rs()
+{
+    let w = window.innerwidth;
+    let h = window.innerheight;
+    let a = window.open("https://www.google.com");
+}
+let btn = document.querySelector("button");
+btn.addEventListener("click", Rs);
